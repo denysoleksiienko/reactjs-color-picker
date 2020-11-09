10d72e01f8606018cb32d317352a23e9f21daf6f
Denis <den.effective@gmail.com>
2020-11-09 22:05:53 +0200

    implement: colors page / router

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,8 +2,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import { ROOT, COLORS } from '../constants/pathnames';
 import { Main } from './Main';
+import { Colors } from './Colors';
 
-const routes = [{ path: ROOT, component: Main, exact: true }];
+const routes = [
+  { path: ROOT, component: Main, exact: true },
+  { path: COLORS, component: Colors, exact: false },
+];
 
 export const Routes = () => {
   return (
